e809e99e14ccc26c922cfd9bd037fa9f0cbaf4b2
Ashutosh Kumar <kumar.ashutosh.9190@gmail.com>
2023-11-20 00:27:36 +0530

    updated files

--- script.js
+++ script.js
@@ -3,31 +3,36 @@ const ipAddressBox = document.querySelector("#ip-address");
 const locationBox = document.querySelector("#location");
 const timezoneBox = document.querySelector("#timezone");
 const ispBox = document.querySelector("#isp");
+const errorBox = document.querySelector(".error-box");
 
-// Function to handle IP address search
+// Function to search client's IP on page load
+const searchClientIP = async () => {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        fetchData(data.ip);
+    } catch (error) {
+        displayError("Failed to fetch your IP and other details.");
+    }
+};
+
+// Function to handle IP address search events
 const searchIPAddress = () => {
     const searchBox = document.querySelector("#search-box");
     const searchBtn = document.querySelector("#search-btn");
 
     document.addEventListener("keypress", (event) => {
         if (event.key === "Enter") {
-            initiateSearch(searchBox.value);
+            fetchData(searchBox.value);
         }
     });
 
     searchBtn.addEventListener("click", () => {
-        initiateSearch(searchBox.value);
+        fetchData(searchBox.value);
     });
 };
 
-const initiateSearch = (ipAddress) => {
-    if (ipAddress.trim() !== "") {
-        fetchData(ipAddress);
-    } else {
-        displayError();
-    }
-};
-
+// Function to fetch data based on IP address
 const fetchData = async (ipAddress) => {
     try {
         const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_z6BIkgf6uQRzUhvLIWsSm9p8TRnPt&ipAddress=${ipAddress}`);
@@ -37,18 +42,22 @@ const fetchData = async (ipAddress) => {
         updateData(ip, location, isp);
         initializeMap(location["lat"], location["lng"], location["city"]);
     } catch (error) {
-        console.error("Error while fetching data!");
-        displayError();
+        displayError("Failed to fetch data. Please check the entered IP address and try again.");
+        ipAddressBox.textContent = locationBox.textContent = timezoneBox.textContent = ispBox.textContent = "N/A";
+        searchClientIP();
     }
 };
 
+// Function to update UI with fetched data
 const updateData = (ipValue, locationValue, ispValue) => {
-    ipAddressBox.textContent = (ipValue != "") ? ipValue : "N/A";
-    locationBox.textContent = (locationValue["region"]) ? locationValue["region"] + ((locationValue["city"]) ? ', ' + locationValue["city"] + ((locationValue["postalCode"]) ? ', ' + locationValue["postalCode"] + "." : ".") : ".") : "N/A";
-    timezoneBox.textContent = (locationValue["timezone"]) ? locationValue["timezone"] : "N/A";
-    ispBox.textContent = (ispValue) ? ispValue : "N/A";
+    const { region, city, postalCode, timezone } = locationValue;
+    ipAddressBox.textContent = ipValue || "N/A";
+    locationBox.textContent = region ? region + (city ? `, ${city}` + (postalCode ? `, ${postalCode}.` : '.') : '.') : 'N/A';
+    timezoneBox.textContent = timezone || "N/A";
+    ispBox.textContent = ispValue || "N/A";
 };
 
+// Function to initialize Leaflet map
 const initializeMap = (latitude, longitude, city) => {
     if (mapContainer._leaflet_id) {
         mapContainer._leaflet_id = null;
@@ -60,19 +69,16 @@ const initializeMap = (latitude, longitude, city) => {
     document.querySelector(".leaflet-control-attribution").remove();
 };
 
-const displayError = () => {
-    const errorBox = document.querySelector(".error-box");
-
+// Function to display and hide errors
+const displayError = (message) => {
     errorBox.style.top = "50%";
-    errorBox.textContent = "Please enter a valid IP Address.";
+    errorBox.innerHTML = message;
     setTimeout(() => {
-        errorBox.textContent = "";
+        errorBox.innerHTML = "";
         errorBox.style.top = "-100%";
     }, 3000);
-
-    mapContainer.innerHTML = "";
-    ipAddressBox.textContent = locationBox.textContent = timezoneBox.textContent = ispBox.textContent = "N/A";
-    mapContainer.innerHTML = ""
 };
 
+// Calling the initial functions
+searchClientIP();
 searchIPAddress();
